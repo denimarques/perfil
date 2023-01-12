51764c7fc28bb76e3f23e8a7bf47cd52cac64b8a
denimarques <101600395+denimarques@users.noreply.github.com>
2023-01-12 10:33:29 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      "Inter": ["Inter", "sans-serif"],
+    },
+    extend: {
+      colors:{
+        'rounds': "#E5E5E5",
+        'space': "#2B2B2B",
+        'ronw': "#767676",
+        'twitr': "#FFB400",
+        'lin': "#F0F0F6",
+      }
+    },
+  },
+  plugins: [],
+}
